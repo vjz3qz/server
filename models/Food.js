fdf43eddeb9f22912380677274581dd82d10d81c
darshdevkar <darshdevkar2021@gmail.com>
2023-03-25 14:09:42 -0400

    changes to food

--- models/Food.js
+++ models/Food.js
@@ -2,9 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FoodSchema = new Schema({
-  name: { type: String, required: true },
-  expirationDate: { type: Date, required: true },
-  restaurant: { type: Schema.Types.ObjectId, ref: 'Restaurant' },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  unit: { 
+    type: String, 
+    required: true 
+  },
+  quantity: { 
+    type: String, 
+    required: true 
+  },
+  expirationDate: { 
+    type: Date, 
+    required: true 
+  },
+  restaurant: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Restaurant' 
+  },
 });
 
 module.exports = mongoose.model('Food', FoodSchema);
