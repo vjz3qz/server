40303ac9caa60b6a321943a0ad59215c974e3970
vjz3qz <vjz3qz@virginia.edu>
2023-06-17 15:52:40 -0400

    cleaned shelter route

--- routes/shelterRoutes.js
+++ routes/shelterRoutes.js
@@ -2,73 +2,79 @@ const express = require('express');
 const router = express.Router();
 const Shelter = require('../models/shelter');
 
-//Get all shelters 
-router.get('/', async(req, res) => {
-    try{
-        const shelters = await Shelter.find();
-        res.json(shelters);
-    } catch(err) {
-        res.json({message: err.message});
-    }
+// Get all shelters
+router.get('/', async (req, res) => {
+  try {
+    const shelters = await Shelter.find();
+    res.json(shelters);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
 });
 
-router.get('/:id',getShelter, (req, res) => {
-    res.json(res.shelter);
-});
-//Get singular shelter
-async function getShelter(req, res, next) {
-    try {
-        shelter = await Shelter.findById(req.params.id);
-        if(shelter == null){
-            return res.json({message: 'Cannot find Shelter'});
-        }else{
-            res.shelter = shelter;
-            next();
-        }
-    } catch(err) {
-        return res.json({message: err.message});
+// Get a singular shelter
+router.get('/:id', async (req, res) => {
+  try {
+    const shelter = await Shelter.findById(req.params.id);
+    if (shelter == null) {
+      return res.json({ message: 'Cannot find Shelter' });
     }
-}
-//Create a new shelter
+    res.json(shelter);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
+
+// Create a new shelter
 router.post('/', async (req, res) => {
-    const shelter = new Shelter({
-        name:req.body.name,
-        location: req.body.location,
-        email: req.body.email
-    });
-    try {
-        const newShelter = await shelter.save();
-        res.json(newShelter);
-    } catch(err){
-        res.json({message: err.message});
-    }
-});
-
-router.patch('/:id', getShelter, async (req, res)=>{
-    if(req.body.name != null){
-        res.shelter.name = req.body.name;
-    }
-    if(req.body.location != null){
-        res.shelter.location = req.body.location;
-    }if(req.body.email != null){
-        res.shelter.email = req.body.email;
-    }
-
-    try{
-        const updatedShelter = await res.shelter.save();
-        res.json(updatedShelter);
-    }catch(err){
-        res.json({message: err.message});
-    }
-});
-//Delete shelter
-router.delete('/:id', getShelter, async (req, res) => {
-    try {
-      await res.shelter.remove();
-      res.json({ message: 'Deleted Shelter' });
-    } catch (err) {
-      res.json({ message: err.message });
-    }
+  const shelter = new Shelter({
+    name: req.body.name,
+    location: req.body.location,
+    email: req.body.email
   });
+  try {
+    const newShelter = await shelter.save();
+    res.json(newShelter);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
 
-  module.exports = router;
+// Update a shelter
+router.patch('/:id', async (req, res) => {
+  try {
+    const shelter = await Shelter.findById(req.params.id);
+    if (shelter == null) {
+      return res.json({ message: 'Cannot find Shelter' });
+    }
+    if (req.body.name != null) {
+      shelter.name = req.body.name;
+    }
+    if (req.body.location != null) {
+      shelter.location = req.body.location;
+    }
+    if (req.body.email != null) {
+      shelter.email = req.body.email;
+    }
+    const updatedShelter = await shelter.save();
+    res.json(updatedShelter);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
+
+// Delete a shelter
+router.delete('/:id', async (req, res) => {
+  try {
+    const shelter = await Shelter.findById(req.params.id);
+    if (shelter == null) {
+      return res.json({ message: 'Cannot find Shelter' });
+    }
+    await shelter.remove();
+    res.json({ message: 'Deleted Shelter' });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
+
+module.exports = router;
